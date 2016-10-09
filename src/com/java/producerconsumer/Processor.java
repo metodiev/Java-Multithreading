6f1b109781687b89b34e3f128358fab999e063d7
metodiev <miroslav.l.metodiev@gmail.com>
2016-10-09 23:25:50 +0300

    Producer consumer

---
+++ src/com/java/producerconsumer/Processor.java
@@ -0,0 +1,30 @@
+package com.java.producerconsumer;
+
+import java.util.Scanner;
+
+public class Processor {
+	
+	public void produce() throws InterruptedException{
+		synchronized (this) {
+			System.out.println("Producer thread running ...");
+			wait();
+			System.out.println("rESUMEND");
+			
+		}
+	}
+	
+	public void consume() throws InterruptedException{
+		
+		Scanner sc = new Scanner(System.in);
+		Thread.sleep(2000);
+		
+		synchronized (this) {
+			System.out.println("Waiting for return key");
+			sc.nextLine();
+			System.out.println("Return key pressed ");
+			notify();
+		Thread.sleep(5000);	
+		}
+	}
+
+}
