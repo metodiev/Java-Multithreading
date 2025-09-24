193b8e528e18d185500b3dd1c405473c1bb9fcbc
metodiev <miroslav.l.metodiev@gmail.com>
2025-09-24 13:54:00 +0300

    1. lifecycle of a Java thread

---
+++ src/com/java/1/thread/lifecycle/states/in/java/ThreadLifecycleDemo.java
@@ -0,0 +1,18 @@
+public class ThreadLifecycleDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(1000); // TIMED_WAITING
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println("State before start: " + t.getState()); // NEW
+        t.start();
+        Thread.sleep(100); // Let it start
+        System.out.println("State after start: " + t.getState()); // RUNNABLE / TIMED_WAITING
+        t.join(); // Waits for termination
+        System.out.println("State after termination: " + t.getState()); // TERMINATED
+    }
+}
